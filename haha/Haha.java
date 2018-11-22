e9f5f9759eb01b2a8c4c044d0a4fea02a7b60ddd
QQ <726379448@qq.com>
2018-11-22 08:54:56 +0800

    添加test Test  calendar

--- haha/Haha.java
+++ haha/Haha.java
@@ -1,10 +1,32 @@
 package com.baidu.haha;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import static java.lang.String.*;
+
 public class Haha {
-    public static void main(String[] args) {
-        System.out.println("hahahahahahha");
-        System.out.println("hahahahahahha");
-        System.out.println("hahahahahahha");
+    public static void main(String[] args) throws Exception {
+        Date date = new Date();
+        /*long time = date.getTime();
+        System.out.println(time);
+        System.out.println("---------------------");
+        date.setTime(999999999);
+        System.out.println(valueOf(date));
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String format = simpleDateFormat.format(date);
+        System.out.println(format);*/
+        Calendar calendar = Calendar.getInstance();
+        int i = calendar.get(Calendar.MONDAY);
+        calendar.set(Calendar.MONDAY,-1);
+        System.out.println(i);
+
+        System.out.println(calendar.get(Calendar.MONDAY));
+        calendar.set(Calendar.MONDAY,-2);
+        System.out.println(calendar.get(Calendar.MONDAY));
+
 
     }
 }
